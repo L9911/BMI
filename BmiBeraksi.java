7f826323de9d07d3a9bc5821f22d499eeff1aca3
L9911 <renleeuwgabriel@gmail.com>
2024-11-01 00:09:09 +0700

    Add files via upload

---
+++ BmiBeraksi.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Ghaxre
+ */
+public class BmiBeraksi {
+    
+   public static void main(String[] args) throws Exception {
+        Bmi NewBmi = new Bmi();
+        NewBmi.getBmi(1.58, 43);
+    }
+}
+
